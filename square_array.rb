0dabceca9814cc4894fa28f1b5ded00caf0403ae
Tyler Carroll <tylercarroll1611@gmail.com>
2019-10-14 22:20:32 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  new_array = []
+  counter = 0  
+  numbers.each do |new_array|
+    new_array << numbers[counter] ** 2 
+    counter += 1  
+  end 
+  new_array
 end
+
