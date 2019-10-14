8356b68d4d6b3a29b8036f3f4a776d2f15dbd8ad
Tyler Carroll <tylercarroll1611@gmail.com>
2019-10-14 23:48:03 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
   new_array = []
   counter = 0  
   numbers.each do |numbers|
-    new_array << numbers[counter] ** 2 
+    new_array << numbers ** 2 
     counter += 1  
   end 
   new_array
